bd72f3797224b3ca54d9881abea881867496e80e
Ace <glen90803@gmail.com>
2021-04-19 15:08:44 +0800

    網頁優化

---
+++ footer.js
@@ -0,0 +1,31 @@
+footer='<div class="footer-light scrolltop animated2 mt-3 border-top RGBA text-light pb-3">\
+<footer>\
+  <div class="container">\
+    <div class="row">\
+      <div class="col-md-4 mt-5">\
+        <h3>聯繫我們</h3>\
+        <p class="mb-1"><i class="fas fa-envelope-square"></i> Email</p>\
+        <p>yongxin@hibox.biz</p>\
+        <p class="mb-1"><i class="fas fa-laptop-house"></i> 地址</p>\
+        <p>433台中市沙鹿區南陽路288號 <a href="page11.html"><i class="fas fa-map-marker-alt"></i></a></p>    \
+      </div>\
+      <div class="col-md-3 mt-auto">\
+        <p class="mb-1"><i class="fas fa-phone"></i> 諮詢專線</p>\
+        <p>04-26310236</p>\
+        <P class="mb-1"><i class="fas fa-fax"></i> 傳真專線</P>\
+        <P>04-22569806</P>\
+      </div>\
+      <div class="col-md-5 mt-auto">\
+        <div class="d-flex align-items-end"><img src="assets/img/mark.png" alt="" style="width: 125px;">\
+          <div class="pb-4 text-left">\
+            <h4>永鑫環保有限公司</h4>\
+            <p>YongXin Environmental Co, Ltd</p>\
+          </div>\
+        </div>\
+      </div>\
+    </div>\
+    <p class="copyright  text-center text-white ">永鑫 © 2021</p>\
+  </div>\
+</footer>\
+</div>';
+document.write(footer);
---
+++ navbar.js
@@ -0,0 +1,51 @@
+navbar = '<nav class="navbar navbar-light navbar-expand-md">\
+<div class="container-fluid">\
+<a href="index.html" class="navbar-brand m-0">\
+<img src = "assets/img/card.png" > </a><button data-toggle="collapse" data-target="#navcol-1"class="navbar-toggler p-0">\
+    <span class="sr-only">Toggle\
+      navigation</span>\
+      <span class="navbar-toggler-icon">\
+      </span>\
+      </button>\
+<div class="collapse navbar-collapse justify-content-around" id="navcol-1">\
+    <ul class="nav navbar-nav mt-4 h4">\
+      <li class="nav-item dropdown ">\
+        <a class="nav-link font-weight-bold mr-2" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"aria-haspopup="true" aria-expanded="false">\
+                公司介紹\
+        </a>\
+        <div class="dropdown-menu border-0" aria-labelledby="navbarDropdown">\
+          <a class="dropdown-item " href="page1.html">公司簡介</a>\
+          <a class="dropdown-item" href="page2.html">組織架構</a>\
+          <a class="dropdown-item" href="#">Something else here</a>\
+        </div>\
+      </li>\
+      <li class="nav-item dropdown">\
+        <a class="nav-link font-weight-bold mr-2" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"aria-haspopup="true" aria-expanded="false">\
+        服務項目\
+        </a>\
+        <div class="dropdown-menu border-0" aria-labelledby="navbarDropdown">\
+          <a class="dropdown-item" href="page3.html">顧問服務</a>\
+          <a class="dropdown-item" href="page4.html">許可業務</a>\
+          <a class="dropdown-item" href="page5.html">申報業務</a>\
+          <a class="dropdown-item" href="page6.html">工程業務</a>\
+          <aclass="dropdown-item" href="page7.html">檢測服務</a>\
+          <a class="dropdown-item" href="page8.html">廢棄物清除服務</a>\
+        </div>\
+      </li>\
+      <li class="nav-item dropdown">\
+        <a class="nav-link font-weight-bold mr-2" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\
+                各類產品\
+        </a>\
+        <div class="dropdown-menu border-0" aria-labelledby="navbarDropdown">\
+          <a class="dropdown-item" href="page9.html">環保用藥</a>\
+          <a class="dropdown-item" href="#">廢棄物相關用藥</a>\
+          <a class="dropdown-item" href="#">環保相關設備</a>\
+        </div>\
+      </li>\
+      <li class="nav-item  "><a class="nav-link font-weight-bold" href="page11.html">\ 聯繫我們</a>\
+      </li>\
+    </ul>\
+  </div>\
+</div >\
+</nav > ';
+document.write(navbar);
